ad0a2bf4811580fdb350ebedfe15510cc16a833b
nickmachnik <nick.machnik@gmail.com>
2022-04-25 14:28:27 +0200

    rm indexed vectors

--- src/bedvec.rs
+++ src/bedvec.rs
@@ -97,7 +97,7 @@ impl BedVecRM {
         }
     }
 
-    pub fn mul_with_vec(&self, v: &[f32]) -> Vec<f32> {
+    pub fn right_multiply(&self, v: &[f32]) -> Vec<f32> {
         (0..self.num_individuals)
             .into_par_iter()
             .map(|row_ix| self.row_dot_product(row_ix, v))
@@ -138,77 +138,6 @@ impl BedVecRM {
     }
 }
 
-pub struct BedVecContig {
-    ixs: Vec<u32>,
-    twos_from: usize,
-}
-
-impl BedVecContig {
-    pub fn new(genotypes: &[u8]) -> Self {
-        let mut ones = Vec::new();
-        let mut twos = Vec::new();
-        for (ix, g) in genotypes.iter().enumerate() {
-            match g {
-                1 => ones.push(ix as u32),
-                2 => twos.push(ix as u32),
-                _ => continue,
-            }
-        }
-        let twos_from = ones.len();
-        twos.iter().for_each(|e| ones.push(*e));
-        Self {
-            ixs: ones,
-            twos_from,
-        }
-    }
-
-    pub fn scaled_add(&self, lhs: &mut [f64], scalar: f64) {
-        self.ixs.iter().enumerate().for_each(|(ix_pos, ix)| {
-            if ix_pos < self.twos_from {
-                lhs[*ix as usize] += scalar
-            } else {
-                lhs[*ix as usize] += scalar + scalar
-            }
-        });
-    }
-}
-
-pub struct BedVec {
-    ones: Vec<u32>,
-    twos: Vec<u32>,
-}
-
-impl BedVec {
-    pub fn new(genotypes: &[u8]) -> Self {
-        let mut ones = Vec::new();
-        let mut twos = Vec::new();
-        for (ix, g) in genotypes.iter().enumerate() {
-            match g {
-                1 => ones.push(ix as u32),
-                2 => twos.push(ix as u32),
-                _ => continue,
-            }
-        }
-
-        Self { ones, twos }
-    }
-
-    pub fn scaled_add(&self, lhs: &mut [f64], scalar: f64) {
-        self.ones.iter().for_each(|ix| lhs[*ix as usize] += scalar);
-        self.twos
-            .iter()
-            .for_each(|ix| lhs[*ix as usize] += scalar + scalar);
-    }
-}
-
-pub fn random_genotype_vec(n: usize, maf: f64) -> Vec<u8> {
-    let mut rng = rand::thread_rng();
-    let bin = Binomial::new(maf, 2).unwrap();
-    (0..n)
-        .map(|_| bin.sample(&mut rng) as u8)
-        .collect::<Vec<u8>>()
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -241,7 +170,7 @@ mod tests {
     }
 
     #[test]
-    fn test_bin_bed_vec_rm_mul_with_vec() {
+    fn test_bin_bed_vec_rm_right_multiply() {
         let num_individuals = 4;
         let num_markers = 4;
         let data: Vec<u8> = vec![0b01001011, 0b11101101, 0b11111110, 0b10110011];
@@ -249,81 +178,55 @@ mod tests {
         let x = BedVecRM::new(data, num_individuals, num_markers);
         assert_eq!(
             vec![3.8616297, -3.0927505, -5.0714474, 4.3025684],
-            x.mul_with_vec(&v)
+            x.right_multiply(&v)
         );
     }
-
-    #[test]
-    fn test_scaled_add() {
-        let n = 100;
-        let maf = 0.2;
-        let w = 0.6;
-        let gtv = random_genotype_vec(n, maf);
-        let expected = gtv.iter().map(|e| *e as f64 * w).collect::<Vec<f64>>();
-        let bv = BedVec::new(&gtv);
-        let mut res_v = vec![0.; n];
-        bv.scaled_add(&mut res_v, w);
-        assert_eq!(res_v, expected);
-    }
-
-    #[test]
-    fn test_scaled_add_contig() {
-        let n = 100;
-        let maf = 0.2;
-        let w = 0.6;
-        let gtv = random_genotype_vec(n, maf);
-        let expected = gtv.iter().map(|e| *e as f64 * w).collect::<Vec<f64>>();
-        let bvc = BedVecContig::new(&gtv);
-        let mut res_v = vec![0.; n];
-        bvc.scaled_add(&mut res_v, w);
-        assert_eq!(res_v, expected);
-    }
 }
 
-#[cfg(all(feature = "unstable", test))]
-mod bench {
-    extern crate test;
+// #[cfg(all(feature = "unstable", test))]
+// mod bench {
+//     extern crate test;
 
-    use super::*;
-    use ndarray::arr1;
-    use test::Bencher;
+//     use super::*;
+//     use ndarray::arr1;
+//     use test::Bencher;
 
-    fn prep() -> (BedVec, BedVecContig, Vec<u8>) {
-        let n = 1000;
-        let maf = 0.2;
-        let gtv = random_genotype_vec(n, maf);
-        let bv = BedVec::new(&gtv);
-        let bvc = BedVecContig::new(&gtv);
-        (bv, bvc, gtv)
-    }
+//     fn prep() -> (BedVec, BedVecContig, Vec<u8>) {
+//         let n = 1000;
+//         let maf = 0.2;
+//         let gtv = random_genotype_vec(n, maf);
+//         let bv = BedVec::new(&gtv);
+//         let bvc = BedVecContig::new(&gtv);
+//         (bv, bvc, gtv)
+//     }
 
-    #[bench]
-    fn bench_scalar_add(b: &mut Bencher) {
-        let w = 0.6;
-        let (bv, _bvc, gtv) = prep();
-        let mut res_v = vec![0.; gtv.len()];
-        b.iter(|| {
-            bv.scaled_add(&mut res_v, w);
-        });
-    }
+//     #[bench]
+//     fn bench_scalar_add(b: &mut Bencher) {
+//         let w = 0.6;
+//         let (bv, _bvc, gtv) = prep();
+//         let mut res_v = vec![0.; gtv.len()];
+//         b.iter(|| {
+//             bv.scaled_add(&mut res_v, w);
+//         });
+//     }
 
-    #[bench]
-    fn bench_scalar_add_contig(b: &mut Bencher) {
-        let w = 0.6;
-        let (_bv, bvc, gtv) = prep();
-        let mut res_v = vec![0.; gtv.len()];
-        b.iter(|| {
-            bvc.scaled_add(&mut res_v, w);
-        });
-    }
+//     #[bench]
+//     fn bench_scalar_add_contig(b: &mut Bencher) {
+//         let w = 0.6;
+//         let (_bv, bvc, gtv) = prep();
+//         let mut res_v = vec![0.; gtv.len()];
+//         b.iter(|| {
+//             bvc.scaled_add(&mut res_v, w);
+//         });
+//     }
 
-    #[bench]
-    fn bench_naive_add(b: &mut Bencher) {
-        let w = 0.6;
-        let (_bv, _bvc, gtv) = prep();
-        let mut res_v = vec![0.; gtv.len()];
-        b.iter(|| {
-            res_v = gtv.iter().map(|e| *e as f64 * w).collect::<Vec<f64>>();
-        });
-    }
-}
+//     #[bench]
+//     fn bench_naive_add(b: &mut Bencher) {
+//         let w = 0.6;
+//         let (_bv, _bvc, gtv) = prep();
+//         let mut res_v = vec![0.; gtv.len()];
+//         b.iter(|| {
+//             res_v = gtv.iter().map(|e| *e as f64 * w).collect::<Vec<f64>>();
+//         });
+//     }
+// }
