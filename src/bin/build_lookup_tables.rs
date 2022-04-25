6efa37f1b06d550cf7bc96d6dfffca50d206bda9
nickmachnik <nick.machnik@gmail.com>
2022-04-25 16:37:54 +0200

    do not use println of empty string

--- src/bin/build_lookup_tables.rs
+++ src/bin/build_lookup_tables.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("//! Lookup tables for unpacking of bed bytes");
-    println!("");
+    println!();
     // genotype value lookups
     println!("/// Lookup table for unpacking of genotype values from bed bytes");
     println!("pub const BED_LOOKUP_GENOTYPE: [f32; 1024] = [");
@@ -18,7 +18,7 @@ fn main() {
         }
     });
     println!("];");
-    println!("");
+    println!();
     // validity lookups
     println!("/// Lookup table for marking valid vs missing data in bed bytes");
     println!("pub const BED_LOOKUP_VALIDITY: [f32; 1024] = [");
@@ -36,7 +36,7 @@ fn main() {
         }
     });
     println!("];");
-    println!("");
+    println!();
     // combined lookups
     println!("/// Lookup table for genotype values and valid data markers");
     println!("pub const BED_LOOKUP_GENOTYPE_AND_VALIDITY: [f32; 2048] = [");
@@ -65,5 +65,5 @@ fn main() {
         }
     });
     println!("];");
-    println!("");
+    println!();
 }
