80e61bee4f6ab63d5bc3a39cdb83cd32b68ca239
nickmachnik <nick.machnik@gmail.com>
2022-04-25 18:19:35 +0200

    rm map reduce benchmark

--- benches/bench_dot_products.rs
+++ benches/bench_dot_products.rs
@@ -4,31 +4,19 @@ use rs_bedvec::bedvec::BedVecCM;
 
 fn rand_bed_column() -> BedVecCM {
     let mut rng = rand::thread_rng();
-    let num_individuals = 10000;
-    let data: Vec<u8> = (0..(num_individuals)).map(|_| rng.gen()).collect();
+    let num_individuals = 10_000;
+    let data: Vec<u8> = (0..(num_individuals / 4)).map(|_| rng.gen()).collect();
     BedVecCM::new(data, num_individuals, 1)
 }
 
-fn bench_bedvec_cm_dot_product_map_reduce(c: &mut Criterion) {
-    let bv = rand_bed_column();
-    let left_w = vec![1.6; bv.num_individuals()];
-    c.bench_function("cm dot map reduce", |b| {
-        b.iter(|| bv.col_dot_product_map_reduce(0, &left_w))
-    });
-}
-
-fn bench_bedvec_cm_dot_product_fold_reduce(c: &mut Criterion) {
+fn bench_bedvec_cm_dot_product(c: &mut Criterion) {
     let bv = rand_bed_column();
     let left_w = vec![1.6; bv.num_individuals()];
     c.bench_function("cm dot fold reduce", |b| {
-        b.iter(|| bv.col_dot_product_fold_reduce(0, &left_w))
+        b.iter(|| bv.col_dot_product(0, &left_w))
     });
 }
 
-criterion_group!(
-    benches,
-    bench_bedvec_cm_dot_product_map_reduce,
-    bench_bedvec_cm_dot_product_fold_reduce
-);
+criterion_group!(benches, bench_bedvec_cm_dot_product);
 
 criterion_main!(benches);
