2838e019a7e7ca2a93c2e7219e9800b963937e73
nickmachnik <nick.machnik@gmail.com>
2022-04-25 16:37:41 +0200

    rm unstable feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
 //! A sparse representation of biallelic genotype data (e.g. as loaded from PLINK .bed files).
-#![cfg_attr(feature = "unstable", feature(test))]
-
 extern crate blas_src;
 extern crate openblas_src;
 
