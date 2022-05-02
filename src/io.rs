da291d94aaf4587714eb7e60390d4f1d5565ce11
nickmachnik <nick.machnik@gmail.com>
2022-05-02 14:55:13 +0200

    impl bed vec reader

---
+++ src/io.rs
@@ -0,0 +1,49 @@
+//! Functionality for reading .bed data from disk
+
+use crate::bedvec::BedVecCM;
+use std::fs::File;
+use std::io::Read;
+
+// For now I expect to read whole .bed files in CM order.
+// If I go for batching, I will probably still be doing that in memory.
+// If not, RM will be necessary for efficient reading of blocks.
+pub struct BedReader {
+    bed_path: String,
+    col_stats_path: Option<String>,
+    num_individuals: usize,
+    num_markers: usize,
+}
+
+impl BedReader {
+    pub fn new(bed_path: &str, num_individuals: usize, num_markers: usize) -> Self {
+        Self {
+            bed_path: bed_path.to_owned(),
+            col_stats_path: None,
+            num_individuals,
+            num_markers,
+        }
+    }
+
+    pub fn with_col_stats(&mut self, col_stats_path: &str) -> &mut Self {
+        self.col_stats_path = Some(col_stats_path.to_owned());
+        self
+    }
+
+    /// Load bedvec from file.
+    ///
+    /// This assumes that the bed file starts with info from the first byte on,
+    /// i.e. that the typical three byte .bed prefix is removed.
+    /// Furthermore, col means and stds are always recomputed, but should
+    /// ideally be stored in accompanying files in preprocessing.
+    ///
+    /// TODO: This could potentially be sped up by memory mapping (memmap) the bed file.
+    /// This could use a lot of memory though if all MarkerGroups do this.
+    pub fn read_into_bedvec(&self) -> BedVecCM {
+        let bytes = std::fs::read(&self.bed_path).expect("failed to read .bed file");
+        if let Some(_p) = &self.col_stats_path {
+            unimplemented!()
+        } else {
+            BedVecCM::new(bytes, self.num_individuals, self.num_markers)
+        }
+    }
+}
