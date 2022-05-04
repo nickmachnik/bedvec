ce2f88683bb5ad1ea8c50ab8aa7df3c0e8818ed5
nickmachnik <nick.machnik@gmail.com>
2022-05-04 13:06:00 +0200

    dd ndarray dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 //! A sparse representation of biallelic genotype data (e.g. as loaded from PLINK .bed files).
+extern crate blas_src;
+extern crate openblas_src;
 
 mod bed_lookup_tables;
 pub mod bedvec;
